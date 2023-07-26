d834fff6e7239918fcbd2ec55b4b52f0bd45917c
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-26 16:29:55 +0300

    Moving check that may cause error from ui to initState

--- qadaa/lib/app/app.dart
+++ qadaa/lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:qadaa/app/modules/onboarding/onboarding.dart';
+import 'package:qadaa/app/shared/functions/print.dart';
 import 'package:qadaa/app/shared/widgets/loading.dart';
 import 'package:qadaa/core/values/constant.dart';
 import 'modules/dashboard/app_dashboard.dart';
@@ -15,13 +16,26 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool isLoading = true;
+  bool isFirstOpen = false;
   @override
   void initState() {
     super.initState();
-
+    checkIfFirstOpen();
     setState(() {
       isLoading = false;
     });
+  }
+
+  void checkIfFirstOpen() {
+    try {
+      final Box box = Hive.box("Prayers");
+      isFirstOpen = box.get(
+        "is_${AppConstant.appVersion}_first_open",
+        defaultValue: true,
+      );
+    } catch (e) {
+      qadaaPrint(e);
+    }
   }
 
   @override
@@ -45,9 +59,7 @@ class _MyAppState extends State<MyApp> {
               fontFamily: "Cairo",
             ),
             home: () {
-              final Box box = Hive.box("Prayers");
-              return box.get("is_${AppConstant.appVersion}_first_open",
-                      defaultValue: true)
+              return isFirstOpen
                   ? const OnBoardingPage()
                   : const AppDashboard();
             }(),
