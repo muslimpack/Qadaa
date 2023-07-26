9376fb79c224f518bc34e6223fb3a520a1d403fe
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-26 20:01:40 +0300

    Add url launch

---
+++ qadaa/lib/app/shared/functions/open_url.dart
@@ -0,0 +1,14 @@
+import 'package:qadaa/app/shared/functions/print.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+Future<void> openURL(String url) async {
+  final customURL = url;
+  final parsed = Uri.parse(customURL);
+  try {
+    if (await canLaunchUrl(parsed)) {
+      await launchUrl(parsed);
+    }
+  } catch (e) {
+    qadaaPrint(e);
+  }
+}
