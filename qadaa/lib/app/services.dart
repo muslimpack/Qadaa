eb76d9fc2783fa875c6da05204920125bdea986c
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-27 15:34:04 +0300

    Update services.dart
    Handle notification permission

--- qadaa/lib/app/services.dart
+++ qadaa/lib/app/services.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
@@ -8,10 +9,20 @@ import 'package:qadaa/core/utils/storage_repo.dart';
 
 Future<void> initServices() async {
   try {
+    await AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+
     //U Doesn't open app notification
     await awesomeNotificationManager.init();
     await awesomeNotificationManager.appOpenNotification();
+  } catch (e) {
+    qadaaPrint(e);
+  }
 
+  try {
     //Hive Initialize
     final appDocumentDirectory =
         await path_provider.getApplicationDocumentsDirectory();
