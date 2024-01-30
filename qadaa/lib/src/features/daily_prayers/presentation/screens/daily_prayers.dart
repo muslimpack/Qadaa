e7750ec7e5fe0840d7f133f0b38439cf6754d594
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-01-30 22:33:13 +0200

    Create daily_prayers.dart

---
+++ qadaa/lib/src/features/daily_prayers/presentation/screens/daily_prayers.dart
@@ -0,0 +1,214 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:qadaa/generated/l10n.dart';
+import 'package:qadaa/src/features/daily_prayers/data/models/meeting.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class DailyPrayers extends StatelessWidget {
+  const DailyPrayers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SfCalendar(
+        showNavigationArrow: true,
+        showTodayButton: true,
+        showWeekNumber: true,
+        showDatePickerButton: true,
+        // allowViewNavigation: true,
+        firstDayOfWeek: DateTime.saturday,
+        view: CalendarView.month,
+        dataSource: MeetingDataSource(_getDataSource()),
+        initialSelectedDate: DateTime.now(),
+        maxDate: DateTime.now(),
+        allowedViews: const <CalendarView>[
+          CalendarView.day,
+          CalendarView.workWeek,
+          CalendarView.week,
+          CalendarView.month,
+          CalendarView.schedule,
+        ],
+        monthViewSettings: const MonthViewSettings(
+          appointmentDisplayMode: MonthAppointmentDisplayMode.none,
+        ),
+        appointmentBuilder: (context, calendarAppointmentDetails) {
+          final Meeting meeting =
+              calendarAppointmentDetails.appointments.first as Meeting;
+          return Card(
+            color: meeting.background,
+            child: Center(
+              child: Text(
+                meeting.eventName,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          );
+        },
+        monthCellBuilder: (context, details) {
+          if (details.appointments.isEmpty &&
+              details.date.isBefore(DateTime.now())) {
+            return Container(
+              margin: const EdgeInsets.all(5),
+              color: Colors.yellow.withOpacity(.1),
+            );
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: details.appointments.map(
+                (e) {
+                  final meeting = e as Meeting;
+                  return Expanded(
+                    child: ColoredBox(
+                      color: meeting.background,
+                      child: Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: FittedBox(
+                          child: Text(
+                            meeting.eventName,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          );
+        },
+        onTap: (calendarTapDetails) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Tapped Date'),
+                content:
+                    Text('You tapped on ${calendarTapDetails.appointments}.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now().subtract(const Duration(days: 2));
+
+    meetings.add(
+      Meeting(
+        S.current.fajr,
+        today.copyWith(hour: 5),
+        today.copyWith(hour: 6),
+        Colors.greenAccent,
+        false,
+      ),
+    );
+    meetings.add(
+      Meeting(
+        S.current.dhuhr,
+        today.copyWith(hour: 12),
+        today.copyWith(hour: 15),
+        Colors.pink,
+        false,
+      ),
+    );
+    meetings.add(
+      Meeting(
+        S.current.asr,
+        today.copyWith(hour: 15),
+        today.copyWith(hour: 17),
+        Colors.greenAccent,
+        false,
+      ),
+    );
+    meetings.add(
+      Meeting(
+        S.current.maghrib,
+        today.copyWith(hour: 17),
+        today.copyWith(hour: 19),
+        Colors.pink,
+        false,
+      ),
+    );
+    meetings.add(
+      Meeting(
+        S.current.ishaa,
+        today.copyWith(hour: 19),
+        today.copyWith(hour: 23, minute: 59, second: 59),
+        Colors.greenAccent,
+        false,
+      ),
+    );
+    return meetings;
+  }
+}
+
+/// An object to set the appointment collection data source to calendar, which
+/// used to map the custom appointment data to the calendar appointment, and
+/// allows to add, remove or reset the appointment collection.
+class MeetingDataSource extends CalendarDataSource {
+  /// Creates a meeting data source, which used to set the appointment
+  /// collection to the calendar
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return _getMeetingData(index).isAllDay;
+  }
+
+  @override
+  String? getNotes(int index) {
+    return "";
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
+  }
+}
