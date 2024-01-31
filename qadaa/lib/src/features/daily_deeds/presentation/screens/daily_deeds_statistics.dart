6bb495996f0c8c5a5122cb9177f25bb7c6911c0c
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-01 00:17:56 +0200

    Create daily_deeds_statistics.dart

---
+++ qadaa/lib/src/features/daily_deeds/presentation/screens/daily_deeds_statistics.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DailyDeedsStatistics extends StatelessWidget {
+  const DailyDeedsStatistics({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
