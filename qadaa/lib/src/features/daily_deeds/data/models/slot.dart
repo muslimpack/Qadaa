14975ff7a2604e27b41ae37433df74d7d3cce80a
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-01-31 06:58:21 +0200

    Update slot.dart

--- qadaa/lib/src/features/daily_deeds/data/models/slot.dart
+++ qadaa/lib/src/features/daily_deeds/data/models/slot.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class Slot {
@@ -21,4 +22,9 @@ class Slot {
   final Color background;
 
   final bool isAllDay;
+
+  @override
+  String toString() {
+    return 'Slot(order: $order, title: $title, from: $from, to: $to, background: $background, isAllDay: $isAllDay)';
+  }
 }
