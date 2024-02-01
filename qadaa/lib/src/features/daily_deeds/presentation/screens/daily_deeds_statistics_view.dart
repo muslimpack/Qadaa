f568db19fec41ce6937735aa8a902dec8dc1a943
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-01 03:30:49 +0200

    Update daily_deeds_statistics_view.dart

--- qadaa/lib/src/features/daily_deeds/presentation/screens/daily_deeds_statistics_view.dart
+++ qadaa/lib/src/features/daily_deeds/presentation/screens/daily_deeds_statistics_view.dart
@@ -2,8 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:qadaa/src/core/shared/loading.dart';
-import 'package:qadaa/src/features/daily_deeds/presentation/components/liquid_circular_progress.dart';
 import 'package:qadaa/src/features/daily_deeds/presentation/components/liquid_linear_progress.dart';
+import 'package:qadaa/src/features/daily_deeds/presentation/components/stats_tile.dart';
 import 'package:qadaa/src/features/daily_deeds/presentation/controller/daily_deeds_stats_controller.dart';
 
 class DailyDeedsStatisticsView extends StatelessWidget {
@@ -31,38 +31,6 @@ class DailyDeedsStatisticsView extends StatelessWidget {
                             ),
                           )
                           .toList(),
-                      //  [
-                      //   Expanded(
-                      //     child: CircularLiquidProgress(
-                      //       value: 1,
-                      //       child: S.of(context).fajr,
-                      //     ),
-                      //   ),
-                      //   Expanded(
-                      //     child: CircularLiquidProgress(
-                      //       value: .5,
-                      //       child: S.of(context).dhuhr,
-                      //     ),
-                      //   ),
-                      //   Expanded(
-                      //     child: CircularLiquidProgress(
-                      //       value: .75,
-                      //       child: S.of(context).asr,
-                      //     ),
-                      //   ),
-                      //   Expanded(
-                      //     child: CircularLiquidProgress(
-                      //       value: .8,
-                      //       child: S.of(context).maghrib,
-                      //     ),
-                      //   ),
-                      //   Expanded(
-                      //     child: CircularLiquidProgress(
-                      //       value: .1,
-                      //       child: S.of(context).ishaa,
-                      //     ),
-                      //   ),
-                      // ],
                     ),
                     Card(
                       child: Padding(
@@ -104,58 +72,6 @@ class DailyDeedsStatisticsView extends StatelessWidget {
                                 ),
                               )
                               .toList(),
-                          //  [
-                          //   StatsTile(
-                          //     label: "ركعتا الفجر",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "الضحى",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "الظهر القبيله",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "الظهر البعديه",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "العصر القبليه",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "المغرب القبليه",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "المغرب البعديه",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "العشاء القبليه",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "العشاء البعديه",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          //   StatsTile(
-                          //     label: "صلاة الليل",
-                          //     count: 50,
-                          //     value: .5,
-                          //   ),
-                          // ],
                         ),
                       ),
                     ),
@@ -163,55 +79,6 @@ class DailyDeedsStatisticsView extends StatelessWidget {
                 ),
               );
       },
-    );
-  }
-}
-
-class StatsTile extends StatelessWidget {
-  final String label;
-  final double value;
-  final int times;
-  final int? count;
-  const StatsTile({
-    super.key,
-    required this.label,
-    required this.value,
-    required this.times,
-    this.count,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 2),
-      child: Row(
-        children: [
-          ConstrainedBox(
-            constraints: const BoxConstraints(minWidth: 100),
-            child: Text(label),
-          ),
-          const SizedBox(width: 10),
-          Expanded(child: LinearLiquidProgress(value: value)),
-          const SizedBox(width: 10),
-          ConstrainedBox(
-            constraints: const BoxConstraints(minWidth: 50),
-            child: Text(
-              "$times",
-              textAlign: TextAlign.center,
-            ),
-          ),
-          if (count != null) ...[
-            const SizedBox(width: 10),
-            ConstrainedBox(
-              constraints: const BoxConstraints(minWidth: 50),
-              child: Text(
-                "$count",
-                textAlign: TextAlign.center,
-              ),
-            ),
-          ],
-        ],
-      ),
     );
   }
 }
