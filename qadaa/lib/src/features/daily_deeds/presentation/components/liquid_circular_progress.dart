40055d08a74465966a412603286cc645078cf25c
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-01 02:32:17 +0200

    Update liquid_circular_progress.dart

--- qadaa/lib/src/features/daily_deeds/presentation/components/liquid_circular_progress.dart
+++ qadaa/lib/src/features/daily_deeds/presentation/components/liquid_circular_progress.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:liquid_progress_indicator_v2/liquid_progress_indicator.dart';
 import 'package:qadaa/src/core/constants/constant.dart';
 
 class LinearLiquidProgress extends StatelessWidget {
@@ -14,25 +13,11 @@ class LinearLiquidProgress extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // height: 5,
-      padding: const EdgeInsets.all(10),
-      child: LiquidLinearProgressIndicator(
-        value: value,
-        valueColor: AlwaysStoppedAnimation(
-          AppConstant.mainColor,
-        ),
-        // backgroundColor: Colors.grey[800],
-        borderColor: AppConstant.mainColor,
-        direction: Axis.vertical,
-        borderWidth: 2,
-        center: Text(
-          child ?? "",
-          style: const TextStyle(
-            fontWeight: FontWeight.bold,
-            // color: Colors.amber,
-          ),
-        ),
+    return LinearProgressIndicator(
+      minHeight: 10,
+      value: value,
+      valueColor: AlwaysStoppedAnimation(
+        AppConstant.mainColor,
       ),
     );
   }
