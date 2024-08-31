719490fcedd14469abebde73c8793b03ce07f59c
7eltantawy <hasaneltantawy@gmail.com>
2024-09-01 01:26:30 +0300

    Update extension_platform.dart

--- qadaa/lib/src/core/extensions/extension_platform.dart
+++ qadaa/lib/src/core/extensions/extension_platform.dart
@@ -18,4 +18,10 @@ extension PlatformExtension on Platform {
   static bool get isPhone {
     return Platform.isAndroid || Platform.isIOS;
   }
+
+  static String? get languageCode {
+    final localeName = Platform.localeName;
+    final String? languageCode = localeName.split("_").firstOrNull;
+    return languageCode;
+  }
 }
